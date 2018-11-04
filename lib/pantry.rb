b1f8993800067bb73140f5b5000cfe8cf5d0a3a6
Leah Miller <lkmiller.87@gmail.com>
2018-11-04 14:49:02 -0700

    Add all tests passing for iteration 1

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,14 @@
 class Pantry
+  attr_reader :stock
+  def initialize
+    @stock = Hash.new(0)
+  end
 
+  def stock_check(food)
+    @stock[food]
+  end
+
+  def restock(food, amount)
+    @stock[food] += amount
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,31 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
 
+  def test_it_starts_with_no_stock
+    pantry = Pantry.new
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_it_can_check_stock
+    pantry = Pantry.new
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_restock_pantry
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    assert_equal 10, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_add_addition_stock
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    pantry.restock("Cheese", 20)
+    assert_equal 30, pantry.stock_check("Cheese")
+  end
 end
